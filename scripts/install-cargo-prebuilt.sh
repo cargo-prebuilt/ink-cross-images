70cd23491d13b7ee75f98da69f99772a14dd67ff
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-03-08 10:45:59 -0500

    Switch cargo-prebuilt from musl to gnu

--- scripts/install-cargo-prebuilt.sh
+++ scripts/install-cargo-prebuilt.sh
@@ -8,14 +8,14 @@ pushd /tmp/prebuilt
 mkdir -p "$CARGO_HOME"/bin
 case "$TARGETARCH" in
 amd64)
-    $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/x86_64-unknown-linux-musl.tar.gz" -o x86_64-unknown-linux-musl.tar.gz
+    $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/x86_64-unknown-linux-gnu.tar.gz" -o x86_64-unknown-linux-gnu.tar.gz
     $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/hashes.sha256" | sha256sum -c --ignore-missing -
-    tar -xzvf x86_64-unknown-linux-musl.tar.gz -C "$CARGO_HOME/bin"
+    tar -xzvf x86_64-unknown-linux-gnu.tar.gz -C "$CARGO_HOME/bin"
     ;;
 arm64)
-    $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/aarch64-unknown-linux-musl.tar.gz" -o aarch64-unknown-linux-musl.tar.gz
+    $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/aarch64-unknown-linux-gnu.tar.gz" -o aarch64-unknown-linux-gnu.tar.gz
     $EXT_CURL_CMD "https://github.com/cargo-prebuilt/cargo-prebuilt/releases/latest/download/hashes.sha256" | sha256sum -c --ignore-missing -
-    tar -xzvf aarch64-unknown-linux-musl.tar.gz -C "$CARGO_HOME/bin"
+    tar -xzvf aarch64-unknown-linux-gnu.tar.gz -C "$CARGO_HOME/bin"
     ;;
 *)
     echo "Unsupported Arch: $TARGETARCH" && exit 1
