a6aa454dfb732877f81ce542b13885716a30ece3
harmless-tech <git@harmless.tech>
2025-09-24 17:40:46 -0400

    Version updates

--- build-args.sh
+++ build-args.sh
@@ -3,14 +3,14 @@
 set -euxo pipefail
 
 {
-	echo "CMAKE_VERSION=3.31.7"
+	echo "CMAKE_VERSION=4.1.1"
 	#echo "OPENSSL_VERSION=openssl-3.5.0"
 	echo "LLVM_VERSION=20"
 	echo "MUSL_VERSION=1.2.5"
 	echo "FREEBSD_MAJOR=13"
 	echo "NETBSD_MAJOR=10"
 	# Bypass openbsd cdn listing a release that is not out. (#36)
-	echo "OPENBSD_MAJOR=7.6"
+	echo "OPENBSD_MAJOR=7.7"
 	# Dynamics
 	echo "BUILD_DATE=$BUILD_DATE"
 	echo "RUST_VERSION=$LATEST_RUST_VERSION"
